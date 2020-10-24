6bf729c655dd610c89b85f43feb5febaab57c7c6
kimlimjustin <kimlimjustin@gmail.com>
2020-10-24 15:30:35 +0700

    Display classes on main page and sidenav

--- client/src/Components/Navbar/home.navbar.js
+++ client/src/Components/Navbar/home.navbar.js
@@ -4,27 +4,56 @@ import DefaultProfile from "../../Icons/profile.png";
 import UserInfo from '../../Library/UserInfo';
 import URL from '../../Static/Backend.url.static';
 import {NavLink} from "react-router-dom";
+import Axios from "axios";
 
 const HomeNavbar = () => {
     const [Profile, setProfile] = useState(null);
+    const [userInfo, setUserInfo] = useState('');
+    const [classes, setClasses] = useState([]);
     useEffect(() => {
         const token = new Cookies().get('token');
         UserInfo(token).then(res => {
             if(res){
-                if(res.profile_picture) setProfile(`${URL}/${res.profile_picture.filename}`)
+                if(res.profile_picture) {setProfile(`${URL}/${res.profile_picture.filename}`); setUserInfo(res)}
             }
         });
-    })
+    }, [])
 
     const openNav = () => document.getElementById("sidenav").style.width = "250px";
 
     const CloseNav = () => document.getElementById("sidenav").style.width = "0px";
 
+    useEffect(() => {
+        if(userInfo){
+            Axios.get(`${URL}/class/get/created/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+            Axios.get(`${URL}/class/get/taught/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+            Axios.get(`${URL}/class/get/studied/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+        }
+    }, [userInfo])
+
     return(
         <nav className='bg-white text-dark topnav'>
             <div id = "sidenav" className="sidenav">
                 <span className="closebtn nav-ham" onClick={CloseNav}>&times;</span>
-                <NavLink to="/">Classes</NavLink>
+                <NavLink to="/" className="sidenav-title">Classes</NavLink>
+                {classes.map(_class => {
+                    return <NavLink to = {`/class/${_class._id}`} key={_class._id} className="sidenav-item">{_class.title}</NavLink>
+                })}
             </div>
             <span className="nav-logo  nav-ham" onClick = {openNav}>☰</span>
             <p className="nav-logo nav-ham" onClick = {() => window.location = "/"}>Classroom</p>
--- client/src/Components/home.component.js
+++ client/src/Components/home.component.js
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from "react";
 import UserInfo from "../Library/UserInfo";
 import Cookies from "universal-cookie";
 import HomeNavbar from "./Navbar/home.navbar";
+import Axios from "axios";
+import URL from "../Static/Backend.url.static";
 
 
 const Home = () => {
     const [userInfo, setUserInfo] = useState('');
+    const [classes, setClasses] = useState([]);
 
     useEffect(() => {
         const token = new Cookies().get('token');
         UserInfo(token).then((res) =>{if(res) setUserInfo(res); else window.location = "/login"})
     }, [])
 
+    useEffect(() => {
+        if(userInfo){
+            Axios.get(`${URL}/class/get/created/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+            Axios.get(`${URL}/class/get/taught/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+            Axios.get(`${URL}/class/get/studied/${userInfo._id}`)
+            .then(res => {
+                res.data.forEach(_class => {
+                    setClasses(classes => [...classes, _class])
+                })
+            })
+        }
+    }, [userInfo])
+
+    //useEffect(() => console.log(classes), [classes])
+
     return(
         <div className="container-fluid">
             <HomeNavbar />
             <div className = "container">
-        
+                {classes.map(_class  => {
+                    return <div className="class box box-shadow" key = {_class._id} onClick = {() => window.location = `/class/${_class._id}`}>
+                        <h1 className="box-title">{_class.title}</h1>
+                        <p className="box-text">{_class.description}</p>
+                    </div>
+                })}
             </div>
         </div>
     )
